7421377a89fa24771a2f865eee0fa5f2716dd55b
KHW24 <rlagpdnjs1266@gmail.com>
2021-02-10 09:36:47 +0900

    sout("4") 제거

--- src/com/cit/login/service/loginService.java
+++ src/com/cit/login/service/loginService.java
@@ -9,7 +9,7 @@ public class loginService {
 	MemberDto mDto = new MemberDto();
 	
 	public int loginCheck(String id, String pw) {
-		System.out.println("4");
+		
 		int result = mDao.loginCheck(id, pw);
 		return result;
 	}
