190e4141bc32bd7b58482833a62b1bb593bbbddd
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2021-02-13 21:16:58 +0900

    Member 잡, 경로 리스트 설정

---
+++ src/com/cit/member/filter/MemberFilter.java
@@ -0,0 +1,37 @@
+package com.cit.member.filter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class MemberFilter
+ */
+@WebFilter(urlPatterns= {"/MemberUpdateController.do", "/joinController.do"})
+public class MemberFilter implements Filter {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		Map<String, String> job = new HashMap<String, String>();
+		Map<String, String> path = new HashMap<String, String>();
+		
+		job.put("학생", "student");
+		job.put("취준생", "predeveloper");
+		job.put("IT 관련 종사자", "developer");
+		
+		path.put("광고","ads");
+		path.put("타싸이트","site");
+		path.put("지인추천","rec");
+		
+		request.setAttribute("job", job);
+		request.setAttribute("path", path);
+		chain.doFilter(request, response);
+	}
+
+}
