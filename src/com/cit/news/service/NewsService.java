106d3a8e7ec0ab29d13a8fcd0b58e67e8b04198f
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2021-02-17 00:22:21 +0900

    글 올리기 모듈 작업

--- src/com/cit/news/service/NewsService.java
+++ src/com/cit/news/service/NewsService.java
@@ -1,7 +1,10 @@
 package com.cit.news.service;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import com.cit.file.model.FileDao;
+import com.cit.file.model.FileDto;
 import com.cit.news.model.NewsDao;
 import com.cit.news.model.NewsDto;
 
@@ -12,8 +15,18 @@ public class NewsService {
 		
 	}
 
-	public int insert(NewsDto ndto) {
+	public int insert(NewsDto ndto, List<FileDto> fList) {
 		NewsDao ndao = NewsDao.getInstance();
-		return ndao.insertNews(ndto);
+		FileDao fdao = FileDao.getInstance();
+		
+		int newsRs = ndao.insertNews(ndto, fList);
+		if( newsRs ==1) {
+			System.out.println("뉴스 정상 삽입");
+			return 1;
+		}else {
+			System.out.println("뉴스 오류");
+			return 0;
+		}
+		
 	};
 }
