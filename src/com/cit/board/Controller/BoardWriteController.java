4c02b552e8c2ad00c42e4d1ca6d46190a875c645
KHW24 <rlagpdnjs1266@gmail.com>
2021-02-17 22:37:25 +0900

    board 작성 코드 완료

--- src/com/cit/board/Controller/BoardWriteController.java
+++ src/com/cit/board/Controller/BoardWriteController.java
@@ -2,7 +2,13 @@ package com.cit.board.Controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +17,13 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.swing.JOptionPane;
 
-@WebServlet("/boardWrite.do")
+import com.cit.board.model.BoardDto;
+import com.cit.board.service.BoardService;
+import com.cit.file.model.FileDto;
+import com.cit.news.model.NewsDto;
+import com.cit.news.service.NewsService;
+
+@WebServlet("/BoardWrite.do")
 public class BoardWriteController extends HttpServlet{
 	
 	@Override
@@ -26,8 +38,45 @@ public class BoardWriteController extends HttpServlet{
 			out.println("</script>");
 			
 		}else {
-			response.sendRedirect("/board/boardWrite.jsp");
+			RequestDispatcher rd = request.getRequestDispatcher("/board/boardWrite.jsp");
+			rd.forward(request, response);
+		}	
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Map<String, String[]> boardParams = new HashMap<String, String[]>();
+		boardParams = request.getParameterMap();
+		BoardDto bdto = new BoardDto();
+		BoardService bs = new BoardService();
+		Set<String> keys = boardParams.keySet();
+
+		Iterator<String> it = keys.iterator();
+
+		while (it.hasNext()) {
+			String key = it.next();
+			String value = boardParams.get(key)[0];
+
+			switch (key) {
+			case "category": bdto.setCate(value);
+				break;
+			case "title": bdto.setTitle(value);
+				break;
+			case "contents": bdto.setCont(value);
+				break;
+			}
 		}
 		
+		String content = boardParams.get("contents")[0];
+		
+		String id = (String) request.getSession().getAttribute("id");
+		bdto.setId(id);
+		
+		List<FileDto> fList = ExtractFiles.makeFileList(content);	// 정규식으로 이미지 태그 추출해서 파일 리스트 만듦
+		bs.insert(bdto, fList);
+		
+	
+		response.sendRedirect(request.getContextPath()+"/BoardList.do");
 	}
+	
 }
