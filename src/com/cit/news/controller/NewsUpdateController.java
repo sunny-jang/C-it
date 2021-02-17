5c9eb5b7da194c21cb02abe4334253c279fc63b8
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2021-02-17 18:52:23 +0900

    뉴스 수정, 삽입 컨트롤러 수정

--- src/com/cit/news/controller/NewsUpdateController.java
+++ src/com/cit/news/controller/NewsUpdateController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.cit.board.Controller.ExtractFiles;
 import com.cit.file.model.FileDto;
 import com.cit.news.model.NewsDto;
 import com.cit.news.service.NewsService;
--- src/com/cit/news/controller/NewsWriteController.java
+++ src/com/cit/news/controller/NewsWriteController.java
@@ -17,11 +17,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.cit.board.Controller.ExtractFiles;
 import com.cit.file.model.FileDto;
 import com.cit.news.model.NewsDto;
 import com.cit.news.service.NewsService;
-
-import oracle.net.aso.i;
 
 /**
  * Servlet implementation class NewsWriteController
