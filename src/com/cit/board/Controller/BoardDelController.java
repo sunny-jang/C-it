bce6e4c2c8e3cffee0ba067975ccc173d31c0b3d
KHW24 <rlagpdnjs1266@gmail.com>
2021-02-17 15:52:15 +0900

    board 글 삭제 코드 추가 완료

---
+++ src/com/cit/board/Controller/BoardDelController.java
@@ -0,0 +1,23 @@
+package com.cit.board.Controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.cit.notice.service.NoticeService;
+
+@WebServlet("/BoardDel.do")
+public class BoardDelController extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int num = Integer.parseInt(request.getParameter("num"));
+		int result = new NoticeService().noticeDel(num);
+		
+		response.sendRedirect(request.getContextPath() + "/BoardList.do");
+	}
+}
